f27fcfbe26a721dfb477085fa701af87d1b5dc7b
camalyak <kaylamac2583@gmail.com>
2026-08-03 16:56:34 -0700

    add mouse_annot

--- scripts/deseq2_stern.R
+++ scripts/deseq2_stern.R
@@ -10,3 +10,9 @@ pacman::p_load(DESeq2)
 # load counts
 path <- ("data/counts_matrix.csv")
 counts <- as.matrix(data.table::fread(path, header = T, colClasses = "integer"), rownames = "gene_name")
+
+
+# load annotations
+apath <- ("data/annot_mouse.csv")
+annot <- data.table::fread(apath, header = TRUE, stringsAsFactors = FALSE, data.table = FALSE)
+rownames(annot) <- annot$GeneID
