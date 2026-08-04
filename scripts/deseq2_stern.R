82b0fa6024eaf96aec385b80cd62888c8e97c327
camalyak <kaylamac2583@gmail.com>
2026-08-04 10:30:05 -0700

    finished DESeq2

--- scripts/deseq2_stern.R
+++ scripts/deseq2_stern.R
@@ -9,10 +9,55 @@ pacman::p_load(DESeq2)
 
 # load counts
 path <- ("data/counts_matrix.csv")
-counts <- as.matrix(data.table::fread(path, header = T, colClasses = "integer"), rownames = "gene_name")
+counts <- as.matrix(data.table::fread(path, header = T, colClasses = "integer"), rownames = "GeneID")
 
 
 # load annotations
 apath <- ("data/annot_mouse.csv")
 annot <- data.table::fread(apath, header = TRUE, stringsAsFactors = FALSE, data.table = FALSE)
 rownames(annot) <- annot$GeneID
+
+
+# sample selection: 
+sm <- "11111111110000000000XXXXXXXXXXXXXXXXXXXX"
+sml <- strsplit(sm, split="")[[1]]
+
+
+# filter out excluded samples (marked as "X")
+sel <- which(sml != "X")
+sml <- sml[sel]
+counts <- counts[ ,sel]
+
+
+
+# group membership for samples
+gs <- factor(sml)
+groups <- make.names(c("9_mo_S","9_mo_A"))
+levels(gs) <- groups
+sample_info <- data.frame(Group = gs, row.names = colnames(counts))
+
+
+# pre-filter low count genes
+# keep genes with at least N counts > 10, where N = size of smallest group
+keep <- rowSums( counts >= 10 ) >= min(table(sml))
+counts <- counts[keep, ]
+
+
+ds <- DESeqDataSetFromMatrix(countData = counts, colData = sample_info, design = ~Group)
+
+ds <- DESeq(ds, test = "Wald", sfType = "poscount")
+
+
+# extract results for top genes table
+r <- results(ds, contrast = c("Group", groups[1], groups[2]), alpha=0.05, pAdjustMethod ="fdr")
+
+
+# save dge file as csv
+r |>
+  as.data.frame() |>
+  tibble::rownames_to_column("GeneID") |>
+  merge(annot[, c("GeneID", "Symbol", "Description")], by = "GeneID", sort = FALSE) |>
+  write.csv("data/DESeq_stern.csv", row.names = FALSE)
+
+
+
--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -5,39 +5,51 @@
 
 
 pacman::p_load(readr,
-               tidyverse)
+               tidyverse,
+               org.Mm.eg.db,
+               AnnotationDbi)
 
 
 # read in counts file
 tbl <- read_tsv("data/4ZJQV2-expression-matrix.tsv")
+symbols <- tbl$gene_name
 
 
-# split by cpm and counts
-# normalized matrix
-cpm_cols <- tbl |> 
-  select(ends_with("_cpm")) |> 
-  names()
+# add GeneID and make rowname = GeneID
+gene_ids <- mapIds(org.Mm.eg.db,
+                   keys = symbols,
+                   column = "ENTREZID",
+                   keytype = "SYMBOL",
+                   multiVals = "first")
 
-cpm_cols_sorted <- cpm_cols[order(as.numeric(str_extract(cpm_cols, "(?<=_)\\d+(?=_cpm)")))]
+tbl$GeneID <- gene_ids
 
-tbl_norm <- tbl |>
-  select(gene_name, all_of(cpm_cols_sorted))
-
-write_csv(tbl_norm, "data/normalized_matrix.csv")
 
 
 # raw counts
 ct_cols <- tbl |>
-  select(ends_with("_count")) |>
+  dplyr::select(ends_with("_count")) |>
   names() 
 
 ct_cols_sorted <- ct_cols[order(as.numeric(str_extract(ct_cols, "(?<=_)\\d+(?=_count)")))]
 
 tbl_ct <- tbl |>
-  select(gene_name, all_of(ct_cols_sorted))
+  dplyr::select(GeneID, all_of(ct_cols_sorted))
+
 
 # for DESeq2, need to convert from dbl to int
-tbl_ct <- tbl_ct |> 
-  mutate(across(ends_with("_count"), ~ as.integer(round(.x))))
+tbl_ct <- tbl_ct |>
+  filter(!is.na(GeneID)) |>
+  group_by(GeneID) |>
+  summarise(across(everything(), sum)) |>
+  column_to_rownames("GeneID") |>
+  as.matrix() |>
+  round()
 
-write_csv(tbl_ct, "data/counts_matrix.csv")
+storage.mode(tbl_ct) <- "integer"
+
+
+tbl_ct |>
+  as.data.frame() |>
+  tibble::rownames_to_column("GeneID") |>
+  write.csv("counts_matrix.csv", row.names = FALSE)
